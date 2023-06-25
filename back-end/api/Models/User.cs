c054b93ec59cfe598e0946fb79f71fa95ea11338
Raeez Adams <raeezadams@MacBook-Air.local>
2023-06-25 11:33:03 +0200

    removed space in folder

--- back-end/api/Models/User.cs
+++ back-end/api/Models/User.cs
@@ -8,10 +8,8 @@ namespace api.Models
         public string UserName { get; set; }
         public string Email { get; set; }
         public int CellNumber { get; set; }
-        public string Password { get; set;
-
+        public string Password { get; set; }
 
 
     }
 }
-
