5af3a11cc87d10ef7b7c9ee417de60d57130b5e9
skhumbuzo311 <mkhskh003@myuct.ac.za>
2023-10-08 22:52:18 +0200

    Minor API updates

--- front-end/src/API/Authentification/Index.ts
+++ front-end/src/API/Authentification/Index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const apiBaseUrl: string = "https://localhost:5187";
+export const apiBaseUrl: string = "http://localhost:5187";
 
 export type User = {
     userName: string;
@@ -9,6 +9,6 @@ export type User = {
     email: string;
 }
 
-export const signup = (user:User) => axios.post(`https://localhost:5187/Authentification/signup`, user).then(response => response.data)
-export const login = (user:User) => axios.post(`https://localhost:5187/Authentification/login`, user).then(response => response.data)
+export const signup = (user:User) => axios.post(`http://localhost:5187/Authentification/signup`, user).then(response => response.data)
+export const login = (user:User) => axios.post(`http://localhost:5187/Authentification/login`, user).then(response => response.data)
 
